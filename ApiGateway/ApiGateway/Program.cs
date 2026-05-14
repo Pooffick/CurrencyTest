9a52ba222070e839526acbb28812d689a2dbe923
Pooffick <pooff1ck@gmail.com>
2026-05-14 18:58:26 +0700

    added ApiGateway and docker files

---
+++ ApiGateway/ApiGateway/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiGateway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+                .AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+            var app = builder.Build();
+
+            app.MapReverseProxy();
+
+            app.Run();
+        }
+    }
+}
